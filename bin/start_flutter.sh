251eb483bb0d9b9e0e4ea4eb2e9b628606de109b
Troels Nielsen <bn.troels@gmail.com>
2023-11-10 18:41:25 +0100

    Start flutter

---
+++ bin/start_flutter.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if command -v enable_flutter.sh > /dev/null; then
+    . enable_flutter.sh
+fi
+cd $(dirname $0)/../flutter
+
+flutter run \
+        --web-port=10100 \
+        --dart-define "LOGIN_REDIRECT=http://localhost:10100" \
+        -d web-server
+
